2c9bbc19135a213b4c87b032513a27e5cbf8c82f
Alok Yadav <yalok6321@gmail.com>
2024-11-08 07:38:12 +0530

    add a single file to export all component

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import AboutPage from "./AboutPage";
+import ContactPage from "./ContactPage";
+import HomePage from "./HomePage";
+import RecipePage from "./RecipePage";
+import SingleRecipe from "./SingleRecipe";
+
+export { HomePage, RecipePage, SingleRecipe, AboutPage, ContactPage };
